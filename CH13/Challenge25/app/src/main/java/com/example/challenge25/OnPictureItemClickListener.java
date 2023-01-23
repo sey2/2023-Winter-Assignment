73e80865045545f1dd651a3c64d0be4226254119
casio2978 <casio2978@naver.com>
2023-01-23 14:26:53 +0900

    [Init] Init (23.01.23)

---
+++ CH13/Challenge25/app/src/main/java/com/example/challenge25/OnPictureItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.challenge25;
+
+import android.view.View;
+
+public interface OnPictureItemClickListener {
+    public void onItemClick(PictureAdapter.ViewHolder holder, View view, int position);
+}
